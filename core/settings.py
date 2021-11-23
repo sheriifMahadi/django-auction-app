c5b6582f1920d0890ae436492325bd43765026c7
sheriifMahadi <sheriffabdulamjid@gmail.com>
2021-11-23 12:01:34 +0000

    .

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
